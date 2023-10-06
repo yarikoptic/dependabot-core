55a6f96b73dc97042d50fc8c3eed2b9abdbd6a16
David Rodríguez <deivid.rodriguez@riseup.net>
2023-10-06 08:21:03 +0200

    Rename locked_version to requested_version
    The locked_version name wrongly suggests that the version is picked up
    from the lockfile.

--- npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
@@ -164,7 +164,7 @@ module Dependabot
       def yarn_version
         return @yarn_version if defined?(@yarn_version)
 
-        @yarn_version = package_manager.locked_version("yarn") || guess_yarn_version
+        @yarn_version = package_manager.requested_version("yarn") || guess_yarn_version
       end
 
       def guess_yarn_version
@@ -176,7 +176,7 @@ module Dependabot
       def pnpm_version
         return @pnpm_version if defined?(@pnpm_version)
 
-        @pnpm_version = package_manager.locked_version("pnpm") || guess_pnpm_version
+        @pnpm_version = package_manager.requested_version("pnpm") || guess_pnpm_version
       end
 
       def guess_pnpm_version
